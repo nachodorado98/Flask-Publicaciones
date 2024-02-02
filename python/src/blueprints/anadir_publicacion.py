15eee7ccbf89f0531ab7ef2bb458094fa07fa62a
nacho dorado <natxo98@gmail.com>
2024-02-02 23:17:04 +0100

    Creacion funcionalidad de anadir publicacion

--- python/src/blueprints/anadir_publicacion.py
+++ python/src/blueprints/anadir_publicacion.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required, current_user
 
+from src.database.conexion import Conexion
 
 bp_anadir_publicacion=Blueprint("anadir_publicacion", __name__)
 
@@ -10,3 +11,24 @@ bp_anadir_publicacion=Blueprint("anadir_publicacion", __name__)
 def anadirPublicacion():
 
 	return render_template("anadir.html", usuario=current_user.username)
+
+@bp_anadir_publicacion.route("/insertar_publicacion", methods=["POST"])
+@login_required
+def insertarPublicacion():
+
+	titulo=request.form.get("titulo")
+	descripcion=request.form.get("descripcion")
+
+	if titulo is None or descripcion is None:
+
+		return redirect("/anadir_publicacion")
+
+	con=Conexion()
+
+	id_usuario=con.obtenerIdUsuario(current_user.username)
+
+	con.insertarPublicacion(id_usuario, titulo, descripcion)
+
+	con.cerrarConexion()
+
+	return redirect("/publicaciones")
--- python/tests/testapp/test_anadir_publicacion.py
+++ python/tests/testapp/test_anadir_publicacion.py
@@ -1,4 +1,6 @@
-def test_pagina_anadir_publicacion_sin_login(cliente):
+import pytest
+
+def test_pagina_anadir_publicacion_sin_login(cliente, conexion):
 
 	respuesta=cliente.get("/anadir_publicacion", follow_redirects=True)
 
@@ -15,9 +17,93 @@ def test_pagina_anadir_publicacion_con_login(cliente, conexion):
 
 		cliente_abierto.post("/login", data={"usuario": "nacho98", "contrasena": "NachoDorado1998&"})
 
-		respuesta=cliente_abierto.get("/anadir_publicacion", follow_redirects=True)
+		respuesta=cliente_abierto.get("/anadir_publicacion")
 
 		contenido=respuesta.data.decode()
 
 		assert respuesta.status_code==200
 		assert "<h1>Nueva Publicacion</h1>" in contenido
+
+def test_pagina_insertar_publicacion_sin_login(cliente, conexion):
+
+	data={"titulo":"Titulo", "descripcion":"Descripcion"}
+
+	respuesta=cliente.post("/insertar_publicacion", data=data, follow_redirects=True)
+
+	contenido=respuesta.data.decode()
+
+	assert respuesta.status_code==200
+	assert "<h1>Iniciar Sesión</h1>" in contenido
+
+@pytest.mark.parametrize(["data"],
+	[
+		({"titulo":"Titulo"},),
+		({"descripcion":"Descripcion"},),
+		({},)
+	]
+)
+def test_pagina_insertar_publicacion_con_login_datos_error(cliente, conexion, data):
+
+	with cliente as cliente_abierto:
+
+		cliente_abierto.post("/singin", data={"nombre":"nacho", "apellido":"dorado", "usuario":"nacho98", "contrasena":"NachoDorado1998&", "edad":25})
+
+		cliente_abierto.post("/login", data={"usuario": "nacho98", "contrasena": "NachoDorado1998&"})
+
+		respuesta=cliente_abierto.post("/insertar_publicacion", data=data)
+
+		contenido=respuesta.data.decode()
+
+		assert respuesta.status_code==302
+		assert respuesta.location=="/anadir_publicacion"
+		assert "<h1>Redirecting...</h1>" in contenido
+
+def test_pagina_insertar_publicacion_con_login(cliente, conexion):
+
+	with cliente as cliente_abierto:
+
+		cliente_abierto.post("/singin", data={"nombre":"nacho", "apellido":"dorado", "usuario":"nacho98", "contrasena":"NachoDorado1998&", "edad":25})
+
+		cliente_abierto.post("/login", data={"usuario": "nacho98", "contrasena": "NachoDorado1998&"})
+
+		data={"titulo":"Titulo", "descripcion":"Descripcion"}
+
+		respuesta=cliente_abierto.post("/insertar_publicacion", data=data, follow_redirects=True)
+
+		contenido=respuesta.data.decode()
+
+		assert respuesta.status_code==200
+		assert "<h1>Publicaciones</h1>" in contenido
+		assert "Nadie ha publicado aun...¡Se el primero en publicar!" not in contenido
+		assert "<h2>Titulo</h2>" in contenido
+		assert "Descripcion" in contenido
+
+	conexion.c.execute("SELECT * FROM publicaciones")
+
+	publicaciones=conexion.c.fetchall()
+
+	assert len(publicaciones)==1
+	assert publicaciones[0]["titulo"]=="Titulo"
+	assert publicaciones[0]["descripcion"]=="Descripcion"
+
+def test_pagina_insertar_publicaciones_con_login(cliente, conexion):
+
+	with cliente as cliente_abierto:
+
+		cliente_abierto.post("/singin", data={"nombre":"nacho", "apellido":"dorado", "usuario":"nacho98", "contrasena":"NachoDorado1998&", "edad":25})
+
+		cliente_abierto.post("/login", data={"usuario": "nacho98", "contrasena": "NachoDorado1998&"})
+
+		data={"titulo":"Titulo", "descripcion":"Descripcion"}
+
+		cliente_abierto.post("/insertar_publicacion", data=data)
+		cliente_abierto.post("/insertar_publicacion", data=data, follow_redirects=True)
+		cliente_abierto.post("/insertar_publicacion", data=data, follow_redirects=True)
+		cliente_abierto.post("/insertar_publicacion", data=data, follow_redirects=True)
+		cliente_abierto.post("/insertar_publicacion", data=data, follow_redirects=True)
+
+	conexion.c.execute("SELECT * FROM publicaciones")
+
+	publicaciones=conexion.c.fetchall()
+
+	assert len(publicaciones)==5
